01118ab85fe059b20647bb6fd0a82ef888d26dee
Jorge Andrés Choque <jorge.ch.999santos@gmail.com>
2022-10-03 03:54:18 -0300

    Palette Display Agregado

--- src/App.js
+++ src/App.js
@@ -1,44 +1,78 @@
-import React, { useEffect, useState } from "react";
-import { getColorsPalettes, getTags } from "./service";
-import "./App.css";
-import Palettes from "./components/Palette/Palettes";
-import Tags from "./components/Tag/Tags";
-import Favorites from "./components/Favorite/Favorites";
-import { FavoritesContext } from "./context/FavoriteContext";
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Home from './routes/Home/Home';
+import PaletteDisplay from './routes/Palette/PaletteDisplay';
+import { useEffect, useState } from 'react';
+import { getColorsPalettes } from './service';
+import { ColorPalettesContext} from './context/ColorPalettesContext'
+
 
 function App() {
   const [colorPalettes, setColorPalettes] = useState([]);
-  const [tags, setTags] = useState([]);
-  const [favorites, setFavorites] = useState([]);
-  useEffect(() => {
+  useEffect(()=> {
     getColorsPalettes()
-      .then((data) => {
-        console.log("data :", data);
-        setColorPalettes(data);
-        setFavorites((data) => data.filter((palette) => palette.liked));
-      })
-      .catch((err) => console.log(err));
-    getTags()
-      .then((data) => {
-        console.log("data :", data);
-        setTags(data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+    .then((data) => {
+      setColorPalettes(data);
+    })
+    .catch((err) => console.log(err));
+  }, [])
+
   return (
-    <FavoritesContext.Provider value={{favorites, setFavorites}}>
-      <div className="App">
-        <header>
-          <h1>Color Pelette Projects</h1>
-        </header>
-        <div className="main-container">
-          <Tags tags={tags} />
-          <Palettes palettes={colorPalettes} />
-          <Favorites favorites={favorites} />
-        </div>
+      <div className='App'>
+        <ColorPalettesContext.Provider value={{colorPalettes, setColorPalettes}}>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/palette/:id' element={<PaletteDisplay/>}/>
+            
+          </Routes>
+        </ColorPalettesContext.Provider>
       </div>
-    </FavoritesContext.Provider>
   );
 }
 
 export default App;
+
+// import React, { useEffect, useState } from "react";
+// import { getColorsPalettes, getTags } from "./service";
+// import "./App.css";
+// import Palettes from "./components/Palette/Palettes";
+// import Tags from "./components/Tag/Tags";
+// import Favorites from "./components/Favorite/Favorites";
+// import { FavoritesContext } from "./context/FavoriteContext";
+
+// function App() {
+//   const [colorPalettes, setColorPalettes] = useState([]);
+//   const [tags, setTags] = useState([]);
+//   const [favorites, setFavorites] = useState([]);
+//   useEffect(() => {
+//     getColorsPalettes()
+//       .then((data) => {
+//         console.log("data :", data);
+//         setColorPalettes(data);
+//         setFavorites((data) => data.filter((palette) => palette.liked));
+//       })
+//       .catch((err) => console.log(err));
+//     getTags()
+//       .then((data) => {
+//         console.log("data :", data);
+//         setTags(data);
+//       })
+//       .catch((err) => console.log(err));
+//   }, []);
+//   return (
+//     <FavoritesContext.Provider value={{favorites, setFavorites}}>
+//       <div className="App">
+//         <header>
+//           <h1>Color Pelette Projects</h1>
+//         </header>
+//         <div className="main-container">
+//           <Tags tags={tags} />
+//           <Palettes palettes={colorPalettes} />
+//           <Favorites favorites={favorites} />
+//         </div>
+//       </div>
+//     </FavoritesContext.Provider>
+//   );
+// }
+
+// export default App;
--- src/components/Palette/Palette.jsx
+++ src/components/Palette/Palette.jsx
@@ -1,54 +1,125 @@
-import React from "react";
-import { useContext, useState } from "react";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
-import { FavoritesContext } from "../../context/FavoriteContext";
-import "./Palette.css";
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FavoritesContext } from '../../context/FavoriteContext';
+import './Palette.css';
 
 const Palette = ({ palette }) => {
-  const { id, name, colors, liked } = palette;
+  const { id, name, colors, liked } = palette
   const { favorites, setFavorites } = useContext(FavoritesContext);
   const [isFavorite, setIsFavorite] = useState(liked);
 
-  const handleFavorites = () => {
+  //TODO tienes que modificar el like en la paleta
+  const handleFavorite = () => {
     setIsFavorite((isFavorite) => !isFavorite);
-    // busco si la paleta ya esta en favoritos
-    const foundIndex = favorites.findIndex((fav) => fav.id === id);
-    console.log('favorites : ',favorites)
-    // para agregar a favoritos
+
+    //busco si la paleta ya esta en favoritos
+    const foundIndex = favorites.findIndex(fav => fav.id === id);
+
+    //para agregar a favoritos
     if (foundIndex === -1) {
-      setFavorites([...favorites, palette]);
-      return;
+      setFavorites([...favorites, palette])
+      return
     }
 
-    // quitar de favoritos
-    setFavorites(favorites.filter((fav) => fav.id !== id));
-  };
+    //Quitar de favoritos
+    setFavorites(
+      favorites.filter((fav) => fav.id !== id )//!==
+    );
+  }
+
+  const copyToClipboard = (value) => {
+    navigator.clipboard.writeText(value)
+  }
+
   return (
-    <div className="palette-container">
-      <div className="palette">
+    <div className='palette-container'>
+      <div className='palette'>
         <h3>{name}</h3>
-        {colors.map((color) => {
+        {colors.map((color, index) => {
           return (
             <div
-              key={color}
-              className="color"
+              key={index}
+              className={`color c${index}`}
               style={{ backgroundColor: color }}
+              onClick={() => copyToClipboard(color)}
             >
-              <span>{color}</span>
+              <div className='hex-code'>{color}</div>
             </div>
           );
         })}
       </div>
-      <div className="fav">
-        {/* { liked ? ( */}
-        {isFavorite ? (
-          <FaHeart className="fav heart" onClick={handleFavorites} />
-        ) : (
-          <FaRegHeart className="fav" onClick={handleFavorites} />
-        )}
+      <div className='palette-actions'>
+        <div className='fav' onClick={handleFavorite}>
+          {isFavorite ? (
+            <FaHeart className='heart'/>
+          ) : (
+            <FaRegHeart/>
+          )}
+        </div>
+        <Link className='btn-see-more' to={`/palette/${id}`}>
+            Ver más
+        </Link>
       </div>
+
     </div>
   );
-};
+}
 
-export default Palette;
+export default Palette
+
+
+// import React from "react";
+// import { useContext, useState } from "react";
+// import { FaHeart, FaRegHeart } from "react-icons/fa";
+// import { FavoritesContext } from "../../context/FavoriteContext";
+// import "./Palette.css";
+
+// const Palette = ({ palette }) => {
+//   const { id, name, colors, liked } = palette;
+//   const { favorites, setFavorites } = useContext(FavoritesContext);
+//   const [isFavorite, setIsFavorite] = useState(liked);
+
+//   const handleFavorites = () => {
+//     setIsFavorite((isFavorite) => !isFavorite);
+//     // busco si la paleta ya esta en favoritos
+//     const foundIndex = favorites.findIndex((fav) => fav.id === id);
+//     console.log('favorites : ',favorites)
+//     // para agregar a favoritos
+//     if (foundIndex === -1) {
+//       setFavorites([...favorites, palette]);
+//       return;
+//     }
+
+//     // quitar de favoritos
+//     setFavorites(favorites.filter((fav) => fav.id !== id));
+//   };
+//   return (
+//     <div className="palette-container">
+//       <div className="palette">
+//         <h3>{name}</h3>
+//         {colors.map((color) => {
+//           return (
+//             <div
+//               key={color}
+//               className="color"
+//               style={{ backgroundColor: color }}
+//             >
+//               <span>{color}</span>
+//             </div>
+//           );
+//         })}
+//       </div>
+//       <div className="fav">
+//         {/* { liked ? ( */}
+//         {isFavorite ? (
+//           <FaHeart className="fav heart" onClick={handleFavorites} />
+//         ) : (
+//           <FaRegHeart className="fav" onClick={handleFavorites} />
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Palette;
